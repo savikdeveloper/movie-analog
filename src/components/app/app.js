26d8eb5c4df9007aada1c0cb0b732b948e1a792b
savikdeveloper <savikdeveloper@gmail.com>
2023-04-29 04:46:14 +0500

    filteration added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,9 @@ class	App extends Component {
 				{ name: 'wednesday', viewers: 12000, favourite: false, like: false, id: 1},
 				{ name: 'Squid game', viewers: 32000, favourite: false, like: false, id: 2},
 				{ name: 'Avatar', viewers: 22000, favourite: false, like: false, id: 3},
-			]
+			],
+			term: '',
+			filter: 'all',
 		}
 	}
 
@@ -65,19 +67,46 @@ class	App extends Component {
 		})
 	}
 
+	searchHandler = (arr, term) => {
+		if(term.length === 0) {
+			return arr;
+		}
+		return arr.filter(item => item.name.toLowerCase().indexOf(term) > -1);
+	}
+
+	updateTermHandler = (term) => {
+		this.setState({term});
+	}
+
+	updateFilterHandler = (filter) => {
+		this.setState({filter})
+	}
+
+	filterHandler = (arr, filter) => {
+		switch (filter){
+			case "popular":
+				return arr.filter(c => c.like)
+			case "mostViewers":
+				return arr.filter(c => c.viewers > 25000)
+			default:
+				return arr;
+		}
+	}
+
 	render() {
-		const { data } = this.state
+		const { data, term, filter } = this.state
 		const allFilmsCount = data.length;
 		const favouriteMovieCount = data.filter(c => c.favourite).length;
+		const visibleDate = this.filterHandler(this.searchHandler(data, term), filter);
 		return(
 			<div className='app font-monospace'>
 				<div className='content'>
 					<AppInfo allFilmsCount={allFilmsCount} favouriteMovieCount={favouriteMovieCount}/>
 					<div className='search-panel'>
-						<SearchPanel />
-						<AppFilter />
+						<SearchPanel updateTermHandler={this.updateTermHandler} />
+						<AppFilter filter={filter} updateFilterHandler={this.updateFilterHandler} />
 					</div>
-					<MovieList onToggleFavourite={this.onToggleFavourite} onToggleLike={this.onToggleLike} data={data} onDelete={this.onDelete}/>
+					<MovieList onToggleFavourite={this.onToggleFavourite} onToggleLike={this.onToggleLike} data={visibleDate} onDelete={this.onDelete}/>
 					<MoviesAddForm addForm={this.addForm} />
 				</div>
 			</div>
