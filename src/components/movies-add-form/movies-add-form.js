ab0f7c5f507ec12b4923b59c59a7d358a8827f71
savikdeveloper <savikdeveloper@gmail.com>
2023-04-29 05:38:16 +0500

    form error complated

--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -18,7 +18,12 @@ class MoviesAddForm extends Component {
 
 	addFormHandler = (e) => {
 		e.preventDefault();
-		this.props.addForm({name: this.state.name, viewers: this.state.views})
+		if(this.state.name !== "" && this.state.views !== ""){
+			this.props.addForm({name: this.state.name, viewers: this.state.views})
+			this.setState({ name: "", views: "" })
+		} else{
+			alert("Iltimos maydonlarni to'ldiring!");
+		}
 	}
 
 	render() {
@@ -29,7 +34,7 @@ class MoviesAddForm extends Component {
 				<h3>Yangi kino qo'shish</h3>
 				<form className='add-form d-flex' onSubmit={this.addFormHandler}>
 					<input 
-						type='text' 
+						type='text'
 						className='form-control new-post-label' 
 						placeholder='Qanday kino?'
 						onChange={this.changeHandlerInput}
